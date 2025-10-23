1504fc94fb32dcc6aff761dd27a73567cb9da310
hm4542608-dot <hm4542608@gmail.com>
2025-10-22 23:27:28 -0700

    Add files via upload

---
+++ python_quiz1.py
@@ -0,0 +1,17 @@
+# input_print_questions.py
+
+# Exercise 1:
+# Ask the user to enter their favorite fruit and print a message.
+print(" Your favorite fruit name is: "  + input(" What is your favorite fruit name? "))
+# Exercise 2:
+# Ask the user to enter the name of their best friend and print a friendship message.
+print(" Your best friend name is:" + input(" Please enter your best friend name? "))
+# Exercise 3:
+# Ask the user what time they wake up and print a sentence.
+print(" You wake up at " + input(" When do you wake up? "))
+# Exercise 4:
+# Ask the user their favorite hobby and print it in a full sentence.
+print(" Your favorite hobby is: " + input(" What is your favorite hobby? ") + ".")
+# Exercise 5:
+# Ask the user what they want to become in the future and print a motivational sentence.
+print(" You will become a great " + input(" What do you want to become in the future? ") + "future")
---
+++ python_quiz2.py
@@ -0,0 +1,39 @@
+# Q1. Create a variable with your full name in small letters.
+# Print it using title().
+my_name = "hassaan mahmood"
+print(my_name.upper())
+# Q2. Create a variable with your madrasah name in small letters.
+# Print it using upper().
+my_madrasah_name = "al-tirmidhi institute"
+print(my_madrasah_name.upper())
+# Q3. Create a city name in mixed letters.
+# Print it using lower().
+my_city_name = "LaHOre"
+print(my_city_name.lower())
+# Q4. Print two student names separated by a tab.
+student_1 = "Ali"
+student_2 = "Zubair"
+print(student_1 + "\t" + student_2)
+# Q5. Print a welcome message, then on the next line print your class name.
+welcome_message = "Good Morning"
+print(welcome_message + "\nMy class name is: 2nd_year")
+# Q6. Print three subjects, each on a new line with a tab before each subject.
+subject1 = "English"
+subject2 = "Urdu"
+subject3 = "Mathematics"
+print("\nSubjects_name:")
+print("\t" + subject1)
+print("\t" + subject2)
+print("\t" + subject3)
+# Q7. Create a variable with extra spaces at both sides for your teacher’s name.
+# Remove spaces using strip() and print.
+my_teacher_name = " Mufti Zubair Sahab "
+print(my_teacher_name.strip())
+# Q8. Create a variable with extra space only on the left for a book title.
+# Remove left space using lstrip() and print.
+book_title = " Stability of Pakistan"
+print(book_title.lstrip())
+# Q9. Create a variable with extra space only on the right for a city name.
+# Remove right space using rstrip() and print.
+city_name = "Lahore "
+print(city_name.strip())
